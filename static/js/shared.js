0d9f5be597db6022f1dd51f3e498349121f1c209
John McLear <john@mclear.co.uk>
2015-01-14 23:54:41 +0000

    dont keep task lists on copy paste where there was no tasklist

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,30 +1,40 @@
 var collectContentPre = function(hook, context){
-  var tname = context.cls;
+  var cls = context.cls;
+  var tname = context.tname;
   var state = context.state; 
   var lineAttributes = state.lineAttributes
 
-  var tagIndex = tname.indexOf("tasklist-not-done");
+  var tagIndex = cls.indexOf("tasklist-not-done");
+  // console.log("tI1", tagIndex);
   if(tagIndex !== -1){
     lineAttributes['tasklist-not-done'] = tags[tagIndex];
   }
 
-  var tagIndex = tname.indexOf("tasklist-done");
+  var tagIndex = cls.indexOf("tasklist-done");
+  // console.log("tI2", tagIndex);
   if(tagIndex !== -1){
     lineAttributes['tasklist-done'] = tags[tagIndex];
   }
+
+  if(tname === "div" || tname === "p"){
+    delete lineAttributes['tasklist-done'];
+    delete lineAttributes['tasklist-not-done'];
+  }
+
 };
 
 var collectContentPost = function(hook, context){
+  var cls = context.cls;
   var tname = context.tname;
   var state = context.state;
   var lineAttributes = state.lineAttributes
 
-  var tagIndex = tname.indexOf("tasklist-not-done");
+  var tagIndex = cls.indexOf("tasklist-not-done");
   if(tagIndex >= 0){
     delete lineAttributes['tasklist-not-done'];
   }
 
-  var tagIndex = tname.indexOf("tasklist-done");       
+  var tagIndex = cls.indexOf("tasklist-done");       
   if(tagIndex >= 0){
     delete lineAttributes['tasklist-done'];
   }
